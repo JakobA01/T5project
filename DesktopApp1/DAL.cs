9e674c5a8a4caca50fbbba7054d693cd5bc0e4c0
JakobA01 <44196278+JakobA01@users.noreply.github.com>
2020-10-23 17:16:26 +0200

    DAL methods

---
+++ DesktopApp1/DAL.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DesktopApp1
+{
+    class DAL
+    {
+        private readonly string connectionString = "Server=(local);Database=Project;Integrated security=true";
+
+        public void CreateTrainer(string tName, int nbrOfBadges)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("create_trainer", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@tName", tName));
+                command.Parameters.Add(new SqlParameter("@nbrOfBadges", nbrOfBadges));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public void CreatePokemon(string pName, string nickName, int pLevel, string pType, int tId)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("create_pokémon", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@pName", pName));
+                command.Parameters.Add(new SqlParameter("@nickName", nickName));
+                command.Parameters.Add(new SqlParameter("@pLevel", pLevel));
+                command.Parameters.Add(new SqlParameter("@pType", pType));
+                command.Parameters.Add(new SqlParameter("@tId", tId));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public void UpdatePokemon(string pName, string nickName, int pId, int pLevel, string pType, int? tId)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("update_pokémon", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@pName", pName));
+                command.Parameters.Add(new SqlParameter("@nickName", nickName));
+                command.Parameters.Add(new SqlParameter("@pLevel", pLevel));
+                command.Parameters.Add(new SqlParameter("@pType", pType));
+                if (tId.HasValue)
+                {
+                    command.Parameters.Add(new SqlParameter("@tId", tId));
+                }
+                else
+                {
+                    command.Parameters.Add(new SqlParameter("@tId", DBNull.Value));
+                }
+                command.Parameters.Add(new SqlParameter("@pId", pId));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public void UpdateTrainer(string tName, int tId, int nbrOfBadges)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("update_trainer", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@tName", tName));
+                command.Parameters.Add(new SqlParameter("@nbrOfBadges", nbrOfBadges));
+                command.Parameters.Add(new SqlParameter("@tId", tId));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public void DeleteTrainer(int tId)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("delete_trainer", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@tId", tId));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public void DeletePokemon(int tId)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("delete_pokémon", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@pId", tId));
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+        }
+        public DataTable ReadTrainer(int tId)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("read_trainer", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@tId", tId));
+                sqlConnection.Open();               
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dt);
+                sqlConnection.Close();
+            }
+            return dt;
+        }
+        public DataTable ReadPokemon(int pId)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("read_pokémon", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@pId", pId));
+                sqlConnection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dt);
+                sqlConnection.Close();
+            }
+            return dt;
+        }
+        public DataTable ReadAllTrainers()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("read_all_trainer", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dt);
+                sqlConnection.Close();
+            }
+            return dt;
+        }
+        public DataTable ReadAllPokemons()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("read_all_pokémon", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dt);
+                sqlConnection.Close();
+            }
+            return dt;
+        }
+    }
+}
--- DesktopApp1/Program.cs
+++ DesktopApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
 
 namespace DesktopApp1
 {
@@ -13,17 +14,22 @@ namespace DesktopApp1
         /// </summary>
         [STAThread]
         static void Main()
-            //HEJ
-
-
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            Console.WriteLine("Hej Jakob");
-
-            //HEJ
+            DAL dal = new DAL();
+            //dal.CreateTrainer("Kim", 8);
+            dal.UpdatePokemon("Pikachu", "Pikapikapika", 2, 30, "Grass", null);
+            dal.UpdateTrainer("Simon Olow", 2, 3);
+            DataTable dt = dal.ReadAllPokemons();
+            foreach(DataRow dr in dt.Rows)
+            {
+                foreach(var item in dr.ItemArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
